d3765afd2ff2e14b0513d59ed649b739dd3e242c
Jessicamf4 <jessicamarquesfialho0@gmail.com>
2022-07-21 18:22:52 -0300

    Dark Mode

---
+++ srcrip.js
@@ -0,0 +1,41 @@
+function trocarModo() {
+    changeClasses();
+    changeText();
+}
+
+function changeClasses(){
+    button.classList.toggle(darkModeClass);
+    h1.classList.toggle(darkModeClass);
+    body.classList.toggle(darkModeClass);
+    footer.classList.toggle(darkModeClass);
+}
+
+function changeText() {
+    const lightMode = "Light Mode";
+    const darkMode = "Dark Mode";
+
+    if(body.classList.contains(darkModeClass)){
+        button.innerHTML = lightMode;
+        h1.innerHTML = darkMode + " ON";
+        return;
+    }
+
+    button.innerHTML = darkMode;
+        h1.innerHTML = lightMode + " ON";
+        return;
+
+
+}
+
+const darkModeClass = 'dark-mode';
+const button = document.getElementById("mode-selector");
+const h1 = document.getElementById("pageLight");
+const body = document.getElementsByTagName("body")[0];
+//body coloca o index dele, como só tem 1 = 0
+const footer = document.getElementsByTagName("footer")[0];
+
+
+
+
+button.addEventListener("click", trocarModo);
+
